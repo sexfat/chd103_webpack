a6d2d2b86b37f315bd97f0cf776e853f2c416d9f
sexfat <bryant6831@gmail.com>
2023-09-15 10:13:41 +0800

    add tiemlinemax

--- src/main.js
+++ src/main.js
@@ -8,12 +8,26 @@ import { gsap } from "gsap";
 $('body').css("background-color" , "red");
 
 //gsap
-gsap.to('.box', {
-   x: 800,
-   y: 600,
-   rotation : 360,
-   duration : 1,
-   scale : 2 
-})
+// gsap.to('.box', {
+//    x: 800,
+//    y: 400,
+//    rotation : 360,
+//    duration : 1,
+//    scale : 6 
+// })
 
+
+const tl =gsap.timeline();  
+
+tl.to('.box' , {
+    x:  400,
+    y: 600,
+    duration : 2,
+    scale: 2
+}).to('.box' , {
+   rotation : 360,
+   repeat : -1
+   
+
+})
 
